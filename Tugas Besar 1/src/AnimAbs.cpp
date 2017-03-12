968f6cf4e514a0793db57edbf66da47efabb5f51
Arzee <rifkyanoa@gmail.com>
2017-03-12 16:12:51 +0700

    added all the files

---
+++ Tugas Besar 1/src/AnimAbs.cpp
@@ -0,0 +1,294 @@
+#include "AnimAbs.h"
+#include <iostream>
+
+using namespace std;
+
+/*Animal Berdasarkan Habitat*/
+
+//Land Animal
+
+//ctor
+LandAnimal::LandAnimal() {}
+
+//cctor
+LandAnimal::LandAnimal(const LandAnimal& la) {}
+
+//dtor
+virtual LandAnimal::~LandAnimal() {}
+
+//operator=
+LandAnimal& LandAnimal::operator=(const LandAnimal& lam) {}
+
+//method eat
+virtual void LandAnimal::Eat() {}
+
+
+//Air Animal
+
+//ctor
+Airnimal::AirAnimal() {}
+
+//cctor
+AirAnimal::AirAnimal(const AirAnimal& a) {}
+
+//dtor
+virtual AirAnimal::~AirAnimal() {}
+
+//operator=
+AirAnimal& AirAnimal::operator=(const AirAnimal& aam) {}
+
+//method eat
+virtual void AirAnimal::Eat() {}
+
+
+//Water Animal
+
+//ctor
+WaterAnimal::WaterAnimal() {}
+
+//cctor
+WaterAnimal::WaterAnimal(const WaterAnimal& wa) {}
+
+//dtor
+virtual WaterAnimal::~WaterAnimal() {}
+
+//operator=
+WaterAnimal& WaterAnimal::operator=(const WaterAnimal& wam) {}
+
+//method eat
+virtual void WaterAnimal::Eat() {}
+
+/*Animal Berdasarkan Famili*/
+
+//Felidae
+
+//ctor
+Felidae::Felidae() {}
+
+//cctor
+Felidae::Felidae(const Felidae& f) {}
+
+//dtor
+virtual Felidae::~Felidae() {}
+
+//operator=
+Felidae& Felidae::operator=(const Felidae& fm) {}
+
+//method eat
+virtual void Felidae::Eat() {}
+
+
+//Giraffidae
+
+//ctor
+Giraffidae::Giraffidae() {}
+
+//cctor
+Giraffidae::Giraffidae(const Giraffidae& g) {}
+
+//dtor
+virtual Giraffidae::~Giraffidae() {}
+
+//operator=
+Giraffidae& Giraffidae::operator=(const Giraffidae& fm) {}
+
+//method eat
+virtual void Giraffidae::Eat() {}
+
+
+//Ursidae
+
+//ctor
+Ursidae::Ursidae() {}
+
+//cctor
+Ursidae::Ursidae(const Ursidae& u) {}
+
+//dtor
+virtual Ursidae::~Ursidae() {}
+
+//operator=
+Ursidae& Ursidae::operator=(const Ursidae& um) {}
+
+//method eat
+virtual void Ursidae::Eat() {}
+
+
+//Equidae
+
+//ctor
+Equidae::Equidae() {}
+
+//cctor
+Equidae::Equidae(const Equidae& e) {}
+
+//dtor
+virtual Equidae::~Equidae() {}
+
+//operator=
+Equidae& Equidae::operator=(const Equidae& em) {}
+
+//method eat
+virtual void Equidae::Eat() {}
+
+
+//Scorpaenidae
+
+//ctor
+Scorpaenidae::Scorpaenidae() {}
+
+//cctor
+Scorpaenidae::Scorpaenidae(const Scorpaenidae& s) {}
+
+//dtor
+virtual Scorpaenidae::~Scorpaenidae() {}
+
+//operator=
+Scorpaenidae& Scorpaenidae::operator=(const Scorpaenidae& sm) {}
+
+//method eat
+virtual void Scorpaenidae::Eat() {}
+
+
+//Delphidae
+
+//ctor
+Delphidae::Delphidae() {}
+
+//cctor
+Delphidae::Delphidae(const Delphidae& d) {}
+
+//dtor
+virtual Delphidae::~Delphidae() {}
+
+//operator=
+Delphidae& Delphidae::operator=(const Delphidae& dm) {}
+
+//method eat
+virtual void Delphidae::Eat() {}
+
+
+//Selachii
+
+//ctor
+Selachii::Selachii() {}
+
+//cctor
+Selachii::Selachii(const Selachii& s) {}
+
+//dtor
+virtual Selachii::~Selachii() {}
+
+//operator=
+Selachii& Selachii::operator=(const Selachii& sm) {}
+
+//method eat
+virtual void Selachii::Eat() {}
+
+
+//Octopodiae
+
+//ctor
+Octopodiae::Octopodiae() {}
+
+//cctor
+Octopodiae::Octopodiae(const Octopodiae& o) {}
+
+//dtor
+virtual Octopodiae::~Octopodiae() {}
+
+//operator=
+Octopodiae& Octopodiae::operator=(const Octopodiae& om) {}
+
+//method eat
+virtual void Octopodiae::Eat() {}
+
+
+//Columbidae
+
+//ctor
+Columbidae::Columbidae() {}
+
+//cctor
+Columbidae::Columbidae(const Columbidae& c) {}
+
+//dtor
+virtual Columbidae::~Columbidae() {}
+
+//operator=
+Columbidae& Columbidae::operator=(const Columbidae& cm) {}
+
+//method eat
+virtual void Columbidae::Eat() {}
+
+
+//Accipitridae
+
+//ctor
+Accipitridae::Accipitridae() {}
+
+//cctor
+Accipitridae::Accipitridae(const Accipitridae& a) {}
+
+//dtor
+virtual Accipitridae::~Accipitridae() {}
+
+//operator=
+Accipitridae& Accipitridae::operator=(const Accipitridae& am) {}
+
+//method eat
+virtual void Accipitridae::Eat() {}
+
+
+//Molossidae
+
+//ctor
+Molossidae::Molossidae() {}
+
+//cctor
+Molossidae::Molossidae(const Molossidae& m) {}
+
+//dtor
+virtual Molossidae::~Molossidae() {}
+
+//operator=
+Molossidae& Molossidae::operator=(const Molossidae& mm) {}
+
+//method eat
+virtual void Molossidae::Eat() {}
+
+
+//Paradisaeidae
+
+//ctor
+Paradisaeidae::Paradisaeidae() {}
+
+//cctor
+Paradisaeidae::Paradisaeidae(const Paradisaeidae& p) {}
+
+//dtor
+virtual Paradisaeidae::~Paradisaeidae() {}
+
+//operator=
+Paradisaeidae& Paradisaeidae::operator=(const Paradisaeidae& pm) {}
+
+//method eat
+virtual void Paradisaeidae::Eat() {}
+
+
+//Felidae
+
+//ctor
+Felidae::Felidae() {}
+
+//cctor
+Felidae::Felidae(const Felidae& f) {}
+
+//dtor
+virtual Felidae::~Felidae() {}
+
+//operator=
+Felidae& Felidae::operator=(const Felidae& fm) {}
+
+//method eat
+virtual void Felidae::Eat() {}
