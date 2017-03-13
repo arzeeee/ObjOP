06977f8d4b4cf70b38572f560dbe17e6f597123e
rizky faramita <rizky.faramitha@gmail.com>
2017-03-13 15:31:49 +0700

    added cage.h

--- VZ03/include/cage.h
+++ VZ03/include/cage.h
@@ -2,7 +2,39 @@
 #define CAGE_H
 
 class Cage {
-  public:
-    Cage();
+public : 
+	//ctor dengan dua parameter integer
+	Cage(int _x,int _y, int _animal_id);
 
-}
+	//cctor
+    Cage(const Cage& c);
+
+    //dtor
+	~Cage();
+
+	//operator=
+	Cage& operator=(const Cage& c);
+
+	//method get X
+	int GetX();
+
+    //method get y
+    int GetY();
+
+    //method get jenis animal
+    int GetAnimal();
+
+    //method get id cage
+    int GetId();
+
+	//method set idx
+	void setCages(int _x,int _y, int _id, int _animal_id);
+
+private:
+	int id;
+	int x;
+	int y;
+    int animal_id;
+};
+
+#endif
---
+++ VZ03/include/cagesnew.h
@@ -0,0 +1,37 @@
+#ifndef CAGESNEW_H
+#define CAGESNEW_H
+
+#include "cage.h"
+
+class Cages {
+public:
+	//ctor tanpa parameter
+	Cages();
+
+	//ctor dengan satu parameter integer
+	Cages(int _neff);
+
+	//cctor
+    Cages(const Cages& c);
+
+    //dtor
+	~Cages();
+
+	//operator=
+	Cages& operator=(const Cages& cs);
+
+	//method get array
+	int GetArrayCage();
+
+    //method get neff
+    int GetNeff();
+
+	//method set idx
+	void SetIdx(int idx, Cage _cage);
+
+private:
+	Cage *array_id;
+	int neff;
+};
+
+#endif
