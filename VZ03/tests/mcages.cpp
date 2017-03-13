9040c8bcbc305da450e68acf0e9322290a6fa930
mahdiarn <diarn_7@live.com>
2017-03-14 00:45:33 +0700

    driver mcages.cpp

--- VZ03/tests/mcages.cpp
+++ VZ03/tests/mcages.cpp
@@ -4,25 +4,18 @@
 using namespace std;
 
 int main () {
+	Cages a;
+	Cages b(1);
+	Cages c(b);
+	Cages d;
 
-	//driver cages, dengan membuat matriks cages ukuran 5*5 
-	//penempatan 1 cage dengan id 1 di titik (1,1) sampai (3,3)
+	d = a;
 
-	Cages tes(5,5);
-	for (int i = 1; i < 4; i++)
-	{
-		for (int j = 1; j < 4; j++)
-		{
-			tes.setIdx(i,j,1);
-		}
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			cout << tes.getIdx(i,j);
-		}
-		cout << endl; 
-	}
-	return 0;
+	Cage *tes;
+
+	*tes = a.GetArrayCage();
+
+	cout << a.GetNeff();
+	Cage be;
+	a.SetIdx(0,be);
 }
