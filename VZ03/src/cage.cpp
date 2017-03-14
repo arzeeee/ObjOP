8b93bfd375544ebbe487fa0bf94dcd0630f5e43e
mahdiarn <diarn_7@live.com>
2017-03-14 07:20:49 +0700

    menghilangkan ctor animal pada prosedure setCage agar menghindari error

--- VZ03/src/cage.cpp
+++ VZ03/src/cage.cpp
@@ -58,49 +58,5 @@ void Cage::setCage(int _x,int _y, int _id, int _animal_id) {
     y = _y;
     id = _id;
     animal_id = _animal_id;
-
-    if (animal_id == 1) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 2) {
-        Harimau H(100,2,false,x,y);
-    } else if (animal_id == 3) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 4) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 5) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 6) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 7) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 8) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 9) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 10) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 11) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 12) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 13) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 14) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 15) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 16) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 17) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 18) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 19) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 20) {
-        Kucing K(10,2,true,x,y);
-    } else if (animal_id == 21) {
-        Kucing K(10,2,true,x,y);
-    }
 }
 
